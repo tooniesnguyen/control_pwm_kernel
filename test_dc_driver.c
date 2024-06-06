10b33011f7cb5f9c9002c9bf531d465f5dfaa45e
tooniesnguyen <nnnhan1882002@gmail.com>
2024-06-06 15:16:08 +0700

    update comment v2

--- test_dc_driver.c
+++ test_dc_driver.c
@@ -1,4 +1,3 @@
-/*CODE BEGIN Includes */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,18 +6,17 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <errno.h> // Include errno header
-/*CODE END Includes  */
 
-/*CODE BEGIN DEFINE */
+//Define device path in /dev direction
 #define DEVICE_PATH "/dev/dc_driver"
+
+//Define cmd for in out control 
 #define SWERVE_MODULE_MAGIC_WORD 's'
 #define SWERVE_IOCTL_SEND_TARGET_SPEED _IOW(SWERVE_MODULE_MAGIC_WORD,DEV_REG_TARGET_SPEED,uint8_t)
 #define SWERVE_IOCTL_SEND_TARGET_ANGLE _IOW(SWERVE_MODULE_MAGIC_WORD,DEV_REG_TARGET_ANGLE,uint8_t)
-#define SWERVE_IOCTL_SET_MODE _IOW(SWERVE_MODULE_MAGIC_WORD,DEV_REG_MODE,uint8_t)
 #define SWERVE_IOCTL_READ_CURRENT_ANGLE _IOR(SWERVE_MODULE_MAGIC_WORD,17,int)
-/*CODE END DEFINE */
 
-/*START INITIALIZING VARIABLES*/
+//Define an enum for module's register
 typedef enum SwerveParamRegister{
 	DEV_REG_SPEED_KP = 1,
 	DEV_REG_SPEED_KI,
@@ -33,73 +31,46 @@ typedef enum SwerveParamRegister{
 	DEV_REG_MODE,
 }SwerveParamRegister;
 
-uint8_t txBuffer[10] = {0};
-uint8_t rxBuffer[10] = {0};
 
+//Define variables for target speed, target angle, and mode
 uint8_t targetSpeed;
 uint8_t targetAngle;
-uint8_t mode_dc;
-uint8_t stage = 0;
-/*END INITIALIZING VARIABLES*/
-
-
-void PutMessage(void* data, uint8_t sizeOfData)
-{
-    memcpy(txBuffer, data, sizeOfData);
-}
-
-void inputBegin(void)
-{
-    printf("Mode 0: Disable motor \n");
-    printf("Mode 1: PID Speed motor \n");
-    printf("Mode 2: PID Position motor \n");
-    printf("Please select mode: \n");
-    
-}
-
-
 
 int main() {
     int fd;
     int readData;
 
-        // Open the device
-    fd = open(DEVICE_PATH, O_RDWR);
-    if (fd < 0) {
-        perror("Failed to open the device");
-        return errno;
-    }
+	// Open the device
+	fd = open(DEVICE_PATH, O_RDWR);
+	if (fd < 0) {
+		perror("Failed to open the device");
+		return errno;
+	}
     
+	//Print the input format for user
     printf("Speed - Angle \n");
+	
     while(1)
     {
-        //scanf("%u %u %u",&mode_dc ,&targetSpeed,&targetAngle);
+        //Get and set the input value for target speed and target angle variables
         scanf("%u %u",&targetSpeed,&targetAngle);
         
+		//In out control with cmd SWERVE_IOCTL_SEND_TARGET_SPEED for sending target speed to module
         if (ioctl(fd, SWERVE_IOCTL_SEND_TARGET_SPEED, &targetSpeed) < 0) {
             perror("Failed to send speed");
             close(fd);
             return errno;
         }
         
-        if (ioctl(fd, SWERVE_IOCTL_SET_MODE, &mode_dc) < 0) {
-            perror("Failed to set mode");
-            close(fd);
-            return errno;
-        }
-        
+		//In out control with cmd SWERVE_IOCTL_SEND_TARGET_ANGLE for sending mode to module
         if (ioctl(fd, SWERVE_IOCTL_SEND_TARGET_ANGLE, &targetAngle) < 0) {
             perror("Failed to send angle");
             close(fd);
             return errno;
-        }
-        
-        
-    
-        
+        } 
     }
     
-    
+    //In out control with cmd SWERVE_IOCTL_READ_CURRENT_ANGLE for reading current angle from module
     /*
     // Read angle from DEV_REG_TARGET_ANGLE
     if (ioctl(fd, SWERVE_IOCTL_READ_CURRENT_ANGLE, &readData) < 0) {
@@ -111,6 +82,7 @@ int main() {
     */
     
     printf("Out of while loop");
+	
     // Close the device
     close(fd);
     return 0;
